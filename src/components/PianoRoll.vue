e9aa3b2cce0a2855092945b61231cad564fe8f45
unknown <michal.olejko2@gmail.com>
2020-11-22 15:35:04 +0100

    Prototype of piano roll

--- src/components/PianoRoll.vue
+++ src/components/PianoRoll.vue
@@ -1,26 +1,77 @@
 <template>
-  <div id="pianoRoll">
-    <table style="width:100%">
-
-  </table>
-
+  <div class="container">
+    <br />
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th></th>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6</th>
+          <th>7</th>
+          <th>8</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr v-for="area in areas" :key="area.id">
+          <td id="firstCol">{{ area.note }}</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-export default{
-  
-}
-// export default {
-//   mounted(){
-
-//   },
-//   methods:{
-
-//   }
-// }
+export default {
+  data() {
+    return {
+      areas: [
+        { note: "B" },
+        { note: "A#" },
+        { note: "A" },
+        { note: "G#" },
+        { note: "G" },
+        { note: "F#" },
+        { note: "F" },
+        { note: "E" },
+        { note: "D#" },
+        { note: "D" },
+        { note: "C#" },
+        { note: "C" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
+table,
+td {
+  border: 1px solid black;
+  width: 100%;
+}
+table {
+  border-collapse: collapse;
+  table-layout: fixed;
+}
+td {
+  height: 20px;
+}
+td:hover {
+  background-color: blue;
+}
+#firstCol {
+  border: 0px;
+}
 </style>
